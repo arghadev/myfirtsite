672491f9cdf815eafcf633cbd075289d558ea39a
arghadev <argha.mallick@indusnet.co.in>
2020-03-04 12:07:33 +0530

    Updates

---
+++ precache-manifest.f2e41e9b6e5fc4c882f70d7a1cc284a5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991279c00d9d0e7ece30f507466b3980",
+    "url": "://arghadev.github.io/myfirtsite/index.html"
+  },
+  {
+    "revision": "564d93567bd5f72c1886",
+    "url": "://arghadev.github.io/myfirtsite/static/css/2.6b514457.chunk.css"
+  },
+  {
+    "revision": "d4807aa9cb345381ecc2",
+    "url": "://arghadev.github.io/myfirtsite/static/css/main.08a07bfa.chunk.css"
+  },
+  {
+    "revision": "564d93567bd5f72c1886",
+    "url": "://arghadev.github.io/myfirtsite/static/js/2.ac98c43c.chunk.js"
+  },
+  {
+    "revision": "05ee83b05f52a0fafc26cfb45bc676ef",
+    "url": "://arghadev.github.io/myfirtsite/static/js/2.ac98c43c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4807aa9cb345381ecc2",
+    "url": "://arghadev.github.io/myfirtsite/static/js/main.fce20e02.chunk.js"
+  },
+  {
+    "revision": "e2cde98c0a21b3b9fd9d",
+    "url": "://arghadev.github.io/myfirtsite/static/js/runtime-main.87db6149.js"
+  },
+  {
+    "revision": "381ba72ecd035e96e6caa3e404f6b793",
+    "url": "://arghadev.github.io/myfirtsite/static/media/demo-banner.381ba72e.jpg"
+  },
+  {
+    "revision": "9e3ba91cf59d075864c2d4824cb7f604",
+    "url": "://arghadev.github.io/myfirtsite/static/media/demo-logo.9e3ba91c.png"
+  },
+  {
+    "revision": "1f934a22dc9a8db7a14cfb2c547c2b95",
+    "url": "://arghadev.github.io/myfirtsite/static/media/demo_film.1f934a22.png"
+  },
+  {
+    "revision": "cf1694ee3b5536199533237fe3e1c866",
+    "url": "://arghadev.github.io/myfirtsite/static/media/person-icon.cf1694ee.png"
+  }
+]);
